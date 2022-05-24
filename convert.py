6e49b0e049c62f8643feffd451bf7ab28963d744
Tianle Chen <tianlechen@gmail.com>
2022-05-23 20:45:52 -0400

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,15 @@
+import pyheif
+from PIL import Image
+
+
+def dump_input_heic_as_jpg(input_path, output_path):
+    heif_file = pyheif.read(input_path)
+    image = Image.frombytes(
+        heif_file.mode,
+        heif_file.size,
+        heif_file.data,
+        "raw",
+        heif_file.mode,
+        heif_file.stride,
+    )
+    image.save(output_path, "JPEG")
