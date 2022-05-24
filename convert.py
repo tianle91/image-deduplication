017241682fb04a22b8584f80ffeb65006638ec8c
Tianle Chen <tianlechen@gmail.com>
2022-05-23 21:04:05 -0400

    convert_inputs

--- convert.py
+++ convert.py
@@ -1,4 +1,11 @@
+import os
+import shutil
+from glob import glob
+from pathlib import Path
+from typing import Dict, Tuple
+
 import pyheif
+import streamlit as st
 from PIL import Image
 
 
@@ -13,3 +20,34 @@ def dump_input_heic_as_jpg(input_path, output_path):
         heif_file.stride,
     )
     image.save(output_path, "JPEG")
+
+
+def convert_inputs(input_directory: str, tempdir: str = 'tempdir') -> Dict[str, Tuple[str, str]]:
+
+    shutil.rmtree(tempdir)
+    os.makedirs(tempdir)
+    filename_to_path_mapping = {}
+
+    files = glob(os.path.join(input_directory, '*'))
+    progress_bar = st.progress(0.)
+
+    for i, input_path in enumerate(files):
+
+        filename = Path(input_path).name
+        filename_extension = filename.split('.')[1].lower()
+        output_filename = filename.split('.')[0] + '.jpg'
+        output_path = os.path.join(tempdir, output_filename)
+
+        if filename_extension == 'heic':
+            dump_input_heic_as_jpg(input_path, output_path)
+        elif filename_extension == 'jpg':
+            shutil.copyfile(input_path, output_path)
+        else:
+            print(f'Ignoring {input_path}')
+            continue
+
+        filename_to_path_mapping[output_filename] = (input_path, output_path)
+        progress_bar.progress(i / len(files))
+
+    progress_bar.empty()
+    return filename_to_path_mapping
