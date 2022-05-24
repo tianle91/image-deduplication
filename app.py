311c0973e3552ce007040ee738eb7cb2cb2dbd54
Tianle Chen <tianlechen@gmail.com>
2022-05-23 21:10:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from imagededup.methods import PHash
 from convert import convert_inputs
 from disjointset import get_grouped_duplicates
 
-inputdir = 'inputdir'
 tempdir = 'tempdir'
 phasher = PHash()
 
-
+st.title('Image Deduplication')
+inputdir = st.text_input(label='Input directory', value='inputdir')
 input_files = glob(os.path.join(inputdir, '*'))
 
 
@@ -28,17 +28,22 @@ def get_mappings_and_grouped_duplicates(input_files, inputdir, tempdir):
 filename_to_path_mapping, grouped_duplicates = get_mappings_and_grouped_duplicates(
     input_files, inputdir, tempdir)
 
-
-with st.form('Deduplication'):
-    remove_original_files = []
-    for k, v in grouped_duplicates.items():
-        with st.expander(f'{k}: {len(v)} duplicates'):
-            for p in v:
-                original_filename, output_filename = filename_to_path_mapping[p]
-                if not st.checkbox(label=original_filename, value=True):
-                    remove_original_files.append(original_filename)
-                st.image(image=output_filename, width=400)
-    if st.form_submit_button():
-        for i, remove_p in enumerate(remove_original_files):
-            st.write(f'[{i}/{len(remove_original_files)}] {remove_p}')
-            os.remove(path=remove_p)
+st.header('Deduplication')
+if len(grouped_duplicates) == 0:
+    st.success('No duplicates found!')
+else:
+    with st.form('Deduplication'):
+        st.text('Uncheck the items you want to remove.')
+        remove_original_files = []
+        for k, v in grouped_duplicates.items():
+            with st.expander(f'{k}: {len(v)} duplicates'):
+                for p in v:
+                    original_filename, output_filename = filename_to_path_mapping[p]
+                    if not st.checkbox(label=original_filename, value=True):
+                        remove_original_files.append(original_filename)
+                    st.image(image=output_filename, width=400)
+        if st.form_submit_button():
+            for i, remove_p in enumerate(remove_original_files):
+                st.write(f'[{i}/{len(remove_original_files)}] {remove_p}')
+                os.remove(path=remove_p)
+            st.success(f'Removed {len(remove_original_files)} files!')
