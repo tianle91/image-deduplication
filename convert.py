d0d7a9b301adff39d5dacb037b29cec3bc62eff5
Tianle Chen <tianlechen@gmail.com>
2023-03-11 08:42:58 -0500

    also check for folder

--- convert.py
+++ convert.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+from typing import Optional
 
 import pyheif
 from PIL import Image
@@ -17,23 +19,26 @@ def read_heic_image(input_filename: str) -> Image.Image:
     return image
 
 
-def read_image(input_filename: str) -> Image.Image:
+def read_image(input_filename: str) -> Optional[Image.Image]:
     filename = Path(input_filename).name
+    if not os.path.isfile(path=input_filename):
+        return None
+
     filename_split = filename.split('.')
     if len(filename_split) != 2:
         print(f'Ignoring {input_filename} because it has no extension.')
         return None
+
+    filename_extension = filename.split('.')[1].lower()
+    if filename_extension in ('mov', 'mp4'):
+        print(f'Ignoring {input_filename} because it is a video.')
+        return None
+    # maybe it's an image at this point
+    if filename_extension == 'heic':
+        return read_heic_image(input_filename=input_filename)
     else:
-        filename_extension = filename.split('.')[1].lower()
-        if filename_extension == 'heic':
-            return read_heic_image(input_filename=input_filename)
-        elif filename_extension in ('mov', 'mp4'):
-            print(f'Ignoring {input_filename} because it is a video.')
-            return None
-        else:
-            try:
-                image = Image.open(input_filename)
-            except Exception:
-                print(f'Ignoring {input_filename} because it cannot be opened.')
-                return None
-        return image
+        try:
+            image = Image.open(input_filename)
+            return image
+        except Exception:
+            print(f'Ignoring {input_filename} because it cannot be opened.')
