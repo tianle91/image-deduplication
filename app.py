a0ce7eda3f959aafa6c131fab03586f032cb1189
tchen-ubuntu <tianlechen@gmail.com>
2024-05-12 16:07:47 +0000

    pageinate

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def get_phash_and_analyzed_status(p: str) -> Optional[Tuple[str, bool]]:
         return db[p]
 
 
-@st.cache_resource(show_spinner=False)
 def get_input_files(inputdir, ignorestrs):
     p = os.path.join(inputdir, "**", "*")
     with st.spinner(
@@ -48,9 +47,7 @@ def get_input_files(inputdir, ignorestrs):
 
 
 @st.cache_resource(show_spinner=False)
-def get_grouped_duplicates(
-    input_files: List[str], eps: float = 0.5
-) -> Dict[int, List[str]]:
+def get_grouped_duplicates(input_files: List[str], eps: float = 0.5) -> List[List[str]]:
     with st.spinner("Analyzing files..."):
         time_start = time.time()
         with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
@@ -79,13 +76,15 @@ def get_grouped_duplicates(
         find_duplicates_time = time.time() - time_start
         logger.warning(f"Finding duplicates took {find_duplicates_time}")
 
-    return grouped_duplicates
+    return [
+        sorted(paths, key=lambda s: len(s)) for paths in grouped_duplicates.values()
+    ]
 
 
 def clean_up_and_stop_app():
-    get_input_files.clear()
+    st.session_state["current_duplication_group"] = 0
     get_grouped_duplicates.clear()
-    st.stop()
+    st.rerun()
 
 
 input_files = []
@@ -111,26 +110,51 @@ with st.sidebar:
     )
 
 
-DEDUPLICATION_README = """
-# Deduplication
-
-Uncheck the items you want to remove.
-- [ ] This will be removed
-- [x] This will not be removed
-"""
-
-
 @st.cache_resource(max_entries=1000)
 def get_preview(p: str) -> Image.Image:
     return get_resized_image(img=read_image(p), max_length=200)
 
 
+def show_duplication_results_and_add_to_deletion(paths: List[str]):
+    with st.form("Add to deletion list"):
+        original_files_to_remove = st.session_state.get("original_files_to_remove", [])
+        st.markdown(f"Resolving {len(paths)} duplicates (uncheck to remove)")
+
+        paths_to_should_delete_mapping = {}
+        for p in paths:
+            size_in_mb = os.path.getsize(p) / (1024**2)
+            paths_to_should_delete_mapping[p] = st.checkbox(
+                label=f"Size: {size_in_mb:.2f} Mb",
+                value=p not in original_files_to_remove,
+                key=p,
+                help="Uncheck to delete",
+            )
+            st.image(image=get_preview(p), caption=p, width=400)
+
+        if st.form_submit_button(label="Add to deletion list"):
+            # sync original_files_to_remove with paths_to_should_delete_mapping
+            for p, checked in paths_to_should_delete_mapping.items():
+                if not checked and p not in original_files_to_remove:
+                    original_files_to_remove.append(p)
+                elif p in original_files_to_remove:
+                    original_files_to_remove.remove(p)
+            st.session_state[
+                "original_files_to_remove"
+            ] = original_files_to_remove.copy()
+            # advance current_duplication_group if possible
+            if not (current_duplication_group + 1 > len(grouped_duplicates) - 1):
+                st.session_state["current_duplication_group"] = (
+                    current_duplication_group + 1
+                )
+                st.rerun()
+            else:
+                st.success("Done! See sidebar for removal confirmation.")
+
+
 if len(input_files) > 0:
     grouped_duplicates = get_grouped_duplicates(input_files=input_files, eps=eps)
-    num_groups = len(grouped_duplicates)
-
     with st.sidebar:
-        st.write(f"Found {num_groups} grouped duplicates.")
+        st.write(f"Found {len(grouped_duplicates)} grouped duplicates.")
         if len(grouped_duplicates) == 0:
             st.warning(
                 "Expected to find duplicates? Try clearing cache and reload page."
@@ -140,59 +164,45 @@ if len(input_files) > 0:
             clean_up_and_stop_app()
 
     if len(grouped_duplicates) > 0:
-        grouped_keys = list(grouped_duplicates.keys())
-        grouped_keys_by_page = [
-            grouped_keys[page_num : min(num_groups, page_num + PAGE_SIZE)]
-            for page_num in range(0, num_groups, PAGE_SIZE)
-        ]
-        num_pages = len(grouped_keys_by_page)
+        current_duplication_group = st.session_state.get("current_duplication_group", 0)
+        st.markdown(
+            f"Viewing Duplication Group: {current_duplication_group} / {len(grouped_duplicates)}"
+        )
 
-        with st.sidebar:
-            current_page = st.selectbox(
-                label=f"Current page (out of {num_pages})",
-                options=list(range(num_pages)),
-            )
-
-        st.markdown(DEDUPLICATION_README)
-        with st.form("select photos to remove for current page"):
-            original_files_to_remove = st.session_state.get(
-                "original_files_to_remove", []
-            )
-
-            page_photo_checked = {}
-            for i, k in enumerate(grouped_keys_by_page[current_page]):
-                v = grouped_duplicates[k]
-                with st.expander(f"{k}: {len(v)} duplicates", expanded=True):
-                    for p in grouped_duplicates[k]:
-                        size_in_mb = os.path.getsize(p) / (1024**2)
-                        page_photo_checked[p] = st.checkbox(
-                            label=f"Size: {size_in_mb:.2f} Mb",
-                            value=p not in original_files_to_remove,
-                            key=p,
-                            help="Uncheck to delete",
-                        )
-                        st.image(image=get_preview(p), caption=p, width=400)
-
-            if st.form_submit_button(label="Add to deletion list"):
-                for p, checked in page_photo_checked.items():
-                    if not checked and p not in original_files_to_remove:
-                        original_files_to_remove.append(p)
-                    elif p in original_files_to_remove:
-                        original_files_to_remove.remove(p)
-                st.session_state[
-                    "original_files_to_remove"
-                ] = original_files_to_remove.copy()
+        l_col, r_col = st.columns(2)
+        with l_col:
+            if st.button(
+                f"Go to {current_duplication_group - 1}",
+                disabled=current_duplication_group - 1 < 0,
+            ):
+                st.session_state["current_duplication_group"] = (
+                    current_duplication_group - 1
+                )
+                st.rerun()
+        with r_col:
+            if st.button(
+                f"Go to {current_duplication_group + 1}",
+                disabled=current_duplication_group + 1 > len(grouped_duplicates) - 1,
+            ):
+                st.session_state["current_duplication_group"] = (
+                    current_duplication_group + 1
+                )
+                st.rerun()
+        # using st.rerun() above avoids current_duplication_group being desynced due to lazy updates
+        show_duplication_results_and_add_to_deletion(
+            paths=grouped_duplicates[current_duplication_group]
+        )
 
     # construct reversed mapping of grouped_duplicates so we can update analyzed status
     p_to_group = {}
-    for group_dex, paths in grouped_duplicates.items():
+    for group_dex, paths in enumerate(grouped_duplicates):
         for p in paths:
             p_to_group[p] = group_dex
 
     with st.sidebar:
         original_files_to_remove = st.session_state.get("original_files_to_remove", [])
-        st.write(f"Will be removing {len(original_files_to_remove)} files.")
-        if st.button("Remove"):
+        st.warning(f"Will be removing {len(original_files_to_remove)} files.")
+        if st.button("Remove", disabled=len(original_files_to_remove) == 0):
             with st.spinner("Removing..."):
                 groups_to_update_status = []
                 progress_bar = st.progress(0.0)
