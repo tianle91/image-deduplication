2c81ad06133a56a8e7fc51950d25e724718758de
tchen-ubuntu <tianlechen@gmail.com>
2024-05-14 04:04:01 +0000

    image is type

--- image_dedup/phash.py
+++ image_dedup/phash.py
@@ -3,10 +3,10 @@ from PIL import Image
 from scipy.fftpack import dct
 
 
-def get_phash(img=Image.Image) -> str:
-    img = img.resize(size=(32, 32), resample=Image.NEAREST).convert('L')
-    img_arr = np.array(img).astype('uint8')
+def get_phash(img: Image.Image) -> str:
+    img = img.resize(size=(32, 32), resample=Image.NEAREST).convert("L")
+    img_arr = np.array(img).astype("uint8")
     dct_arr = dct(dct(img_arr, axis=0), axis=1)[:8, :8]
     dct_median = np.median(np.ndarray.flatten(dct_arr)[1:])
     phash_arr = dct_arr < dct_median
-    return ''.join('%0.2x' % x for x in np.packbits(phash_arr))
+    return "".join("%0.2x" % x for x in np.packbits(phash_arr))
